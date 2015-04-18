2bb8fb8c52e37808b1960b940eb1f09fe3b5cedd
gfgtdf <gfgtdf@users.noreply.github.com>
2015-04-19 00:39:32 +0200

    fixup HEAD

--- lua/trader_list_mp.lua
+++ lua/trader_list_mp.lua
@@ -76,9 +76,8 @@ table.insert(trader_list, {
 	price = 10, 
 	image = "units/robots/robot_small.png",
 	apply_func = function(inv)
-		inv.add_random_items_from_comma_seperated_list("bombdropper,trapper_modifier,cooling_addon,heating_addon,spear_fire_modier", 1)
+		inv.add_random_items_from_comma_seperated_list("bombdropper,trapper_modifier,cooling_addon,heating_addon,spear_fire_modier,antenna", 1)
 		inv.add_random_items_from_comma_seperated_list("four_parted_healing_sw_w,four_parted_healing_nw_w,four_parted_healing_ne_e,four_parted_healing_es_e", 1)
-		inv.add_amount("antenna",10)
 	end
 })
 return trader_list
