736f39e3ff68c1dd34396bebe506dd464e60a4fd
gfgtdf <gfgtdf@users.noreply.github.com>
2016-01-31 19:46:46 +0100

    remove a global variable

--- lua/global_events.lua
+++ lua/global_events.lua
@@ -41,7 +41,6 @@ global_events.init = function()
 	for i,side in ipairs(wesnoth.sides) do
 		inventories[side.side] = Inventory.new("component_inventory_" .. tostring(side.side))
 	end
-	inventory = inventories[1]
 	traps = Traps.new()
 	traps.init()
 	current_event_name = ""
