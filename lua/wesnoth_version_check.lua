0e3595c289d35047110c02ee50b35dea6056abbd
gfgtdf <daniel.gfgtdf@gmail.com>
2016-07-21 02:21:53 +0200

    update and fix wesnoth version check

--- lua/wesnoth_version_check.lua
+++ lua/wesnoth_version_check.lua
@@ -1,7 +1,7 @@
-local last_tested_version = "1.13.4+dev"
+local last_tested_version = "1.13.5"
 local min_wesnoth_version = "1.12"
 global_events.add_event_handler("prestart", function(event_context)
-	if wesnoth.compare_versions(wesnoth.game_config.version, ">=", last_tested_version) then
+	if wesnoth.compare_versions(wesnoth.game_config.version, ">", last_tested_version) then
 		wesnoth.wml_actions.message {
 			speaker = "narrator",
 			message = "'Scenario with robots' was only tested up to wesnoth version  " .. last_tested_version .. ". It might not work on your wesnoth version which is " .. wesnoth.game_config.version .. ".",
